a792646a80fb3e7f9dc44a2d38e91db433f44e4b
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-02 12:01:29 +0200

    added added a form

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req,res) =>{
     const url = req.url;
     const method = req.method;
-
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
+        res.write('<head><title>Enter Msg</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
         res.write('</html>');
         return res.end();
     }
-
-    if (url === '/message' && method === 'POST') {
+    if(url === '/message' && method === 'POST'){
         const body = [];
-        req.on('data', (chunk) => {
+        req.on('data', (chunk)=> {
+            console.log(chunk);
             body.push(chunk);
         });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+        req.on('end', () =>{
+              const parsedBody = Buffer.concat(body).toString();
+              const message = parsedBody.split('=')[1];
+              fs.writeFileSync('message.txt', message);
         });
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end(); 
     }
-
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my node, it\'s Wayne!</h1></body>');
+    res.write('<head><title>My first page</title><head>');
+    res.write('<body><h1>Hello fom my node its wayne!');
     res.write('</html>');
     res.end();
-});
-
-server.listen(3000);
+})
+server.listen(2000); 
