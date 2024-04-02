7f5d2901d54417e9ce17c1ca1ea724ab3b4da8bb
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-02 14:40:42 +0200

    added a routes.js and pass in all the data

---
+++ routes.js
@@ -0,0 +1,32 @@
+if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Msg</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
+    res.write('</html>');
+    return res.end();
+}
+if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk)=> {
+        console.log(chunk);
+        body.push(chunk);
+    });
+    req.on('end', () =>{
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split('=')[1];
+          fs.writeFileSync('message.txt', message, (err) => {
+            
+          });
+    });
+    fs.writeFileSync('message.txt', 'Dummy');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end(); 
+}
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>My first page</title><head>');
+res.write('<body><h1>Hello fom my node its wayne!');
+res.write('</html>');
+res.end();
