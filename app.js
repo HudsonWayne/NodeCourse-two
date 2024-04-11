870e7412a1fe052d3e9bdf4dada7d3ba374b6c5f
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-11 10:42:53 +0200

    added my button

--- app.js
+++ app.js
@@ -7,35 +7,68 @@
 // server.listen(2000); 
 
 
-const express = require('express');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-// Create the app instance before using it
-const app = express();
+// // Create the app instance before using it
+// const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('Hello World from Express');
-  // ......
-});
+// app.use('/', (req, res, next) => {
+//   console.log('In another middleware!');
+//   res.send('Hello World from Express');
+//   // ......
+// });
 
-app.use('/add-product', (req, res, next) => {
-  console.log('This always runs!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-  // ......
-});
+// app.use('/add-product', (req, res, next) => {
+//   console.log('This always runs!');
+//   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+//   // ......
+// });
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-})
+// app.use('/product', (req, res, next) => {
+//   console.log(req.body);
+//   res.redirect('/');
+// })
 
-// Require the http module
+// // Require the http module
+// const http = require('http');
+
+// // Create the server using http.createServer(app)
+// const server = http.createServer(app);
+
+// server.listen(8080);
+
 const http = require('http');
 
-// Create the server using http.createServer(app)
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next) =>{
+  console.log('This always runs!');
+  next();
+})
+
+app.use('/add-product',(req, res, next) =>{
+  console.log('In another middleware');
+  res.send('<form action="/product" method="Post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+})
+
+app.use('/product', (req, res, next) =>{
+
+    console.log(res.body); 
+    res.redirect('/');
+})
+
+
+app.use('/', (req, res, next) =>{
+  console.log('In another middleware');
+  res.send('<h1>Hello from Node</h1>');
+})
+
+
 const server = http.createServer(app);
 
-server.listen(8080);
+server.listen(7000)
