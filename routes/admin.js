262f539e2ee63f3894445e13b88c87a19a69cb92
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-15 13:23:22 +0200

    serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 // Route for displaying the product form
 router.get('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-  });
+   
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
   
   // Route for handling product submission (assuming you have logic for saving the product)
    router.post('/add-product', (req, res, next) => {
