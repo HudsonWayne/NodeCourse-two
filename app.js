6b3d0bdaa137b4ccc0024bdccd25fbee59006039
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-15 14:15:32 +0200

    Recalling Page 404 Not Found

--- app.js
+++ app.js
@@ -39,6 +39,7 @@
 // const server = http.createServer(app);
 
 // server.listen(8080);
+const path = require('path')
 const http = require('http');
 
 const bodyParser = require('body-parser'); // Assuming Express version below 4.16
@@ -61,7 +62,7 @@ app.use(shopRoutes);
 
 
 app.use((req,res, next)=>{
-  res.status(404).send('<h1>Page not found</h1');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
