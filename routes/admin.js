5e33ac896e8e94eb1d51c1748223e0a0334403aa
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-15 15:30:09 +0200

    added a utils dir

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 // Route for displaying the product form
