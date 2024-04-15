24af727203702ea8dbfa624d9a1e3feac0dd02c6
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-15 11:42:25 +0200

    added filtering path two

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Route for displaying the product form
 router.get('/add-product', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
   });
   
   // Route for handling product submission (assuming you have logic for saving the product)
