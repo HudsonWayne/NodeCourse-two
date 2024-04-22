4cdf5b1953dc5d1a1334dd894d49cf98dcbd5bcd
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-22 15:04:06 +0200

    working with handlebars for dynamic routing

--- app.js
+++ app.js
@@ -1,65 +1,33 @@
-// const http = require('http');
-// const routes = require('./routes')
-
-// console.log(routes.someText);
-
-// const server = http.createServer(routes, handler);
-// server.listen(2000); 
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// // Create the app instance before using it
-// const app = express();
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use('/', (req, res, next) => {
-//   console.log('In another middleware!');
-//   res.send('Hello World from Express');
-//   // ......
-// });
-
-// app.use('/add-product', (req, res, next) => {
-//   console.log('This always runs!');
-//   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-//   // ......
-// });
-
-// app.use('/product', (req, res, next) => {
-//   console.log(req.body);
-//   res.redirect('/');
-// })
-
-// // Require the http module
-// const http = require('http');
-
-// // Create the server using http.createServer(app)
-// const server = http.createServer(app);
-
-// server.listen(8080);
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars'); // Corrected import
 
 const app = express();
 
-app.set('view engine', 'pug');
+const hbs = expressHbs(); // Create an instance (optional for default config)
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
+
+// ... rest of your code
+
+
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+// Error handling middleware (404 Not Found)
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(7000);
+
