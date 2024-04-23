f06696763e4cbf2a58f53bf1e7834c8f486710ca
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-23 09:43:08 +0200

    added shop.js router

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+// const path = require('path');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const expressHbs = require('express-handlebars'); // Corrected import
+
+// const app = express();
+
+// const hbs = expressHbs(); // Create an instance (optional for default config)
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+// app.set('views', 'views');
+
+// // ... rest of your code
+
+
+
+// const adminData = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/admin', adminData.routes);
+// app.use(shopRoutes);
+
+// // Error handling middleware (404 Not Found)
+// app.use((req, res, next) => {
+//   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
+
+// app.listen(7000);
+
+
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -5,15 +39,11 @@ const expressHbs = require('express-handlebars'); // Corrected import
 
 const app = express();
 
-const hbs = expressHbs(); // Create an instance (optional for default config)
+const hbs = expressHbs; // Create an instance (optional for default config)
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', 'views');
-
-// ... rest of your code
-
-
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -29,5 +59,7 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(7000);
+app.listen(7000, () => {
+  console.log('Server is running on port 7000');
+});
 
