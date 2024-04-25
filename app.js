5b8a87273af0221c05ab7145e91affdd58e0b0c7
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-25 08:46:05 +0200

    adde the maind-handle.pug

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs() );
+app.engine('hbs', expressHbs({layoutDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}) );
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
@@ -24,4 +24,4 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(7000);
+app.listen(4000);
