db64637caeeb4c3ed768114339a7eee56bd25f96
HudsonWayne <waynewaynebenhura@gmail.com>
2024-04-30 15:19:18 +0200

    added a file

--- models/product.js
+++ models/product.js
@@ -1,3 +1,7 @@
+const { log } = require('console');
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
 
 module.exports = class Product {
@@ -6,7 +10,17 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+       
+        console.log(fileContent);
+        
+    }
+
+)
+
+        
     }
 
     static fetchAll() {
