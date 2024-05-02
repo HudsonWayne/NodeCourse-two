6ef0e85182f42fc1062d0d95d51844b938781152
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-02 10:12:59 +0200

    storing data in files via the model

--- models/product.js
+++ models/product.js
@@ -13,9 +13,14 @@ module.exports = class Product {
         const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json'
     );
     fs.readFile(p, (err, fileContent) => {
-       
-        console.log(fileContent);
-        
+        let products = [];
+        if (!err) {
+            products = JSON.parse(fileContent);
+        }
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+            console.log(err);
+        });
     }
 
 )
