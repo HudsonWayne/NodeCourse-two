b0fa31a3632bb8840ec6fd8a8d6fbc9e5c80e87d
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-03 12:55:33 +0200

    added mysql course fully

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'nodecourse',
-    password: '1234',
-})
+    database: 'node-complete',
+    password: 'nodecomplete'
+});
 
 module.exports = pool.promise();
