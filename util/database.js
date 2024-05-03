c660adda9cf653d307bcbcadaee1316357509808
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-03 13:49:30 +0200

    upgraded mysql to latest

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: '1234'
 });
 
 module.exports = pool.promise();
